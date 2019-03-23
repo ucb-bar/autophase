268b31510bd180303a3e219e3f69f53fa81f4d23
Jenny Huang <hqjenny@hotmail.com>
2019-03-23 04:50:41 -0700

    Add ppo

--- algos/rl/ppo_ray.py
+++ algos/rl/ppo_ray.py
@@ -8,27 +8,29 @@ ray.init()
 env_configs = {}
 
 num_pgms = 1
-from gym_hls.envs.chstone_bm import get_chstone, get_others
-bms = get_chstone(N=num_pgms)
+from gym_hls.envs.chstone_bm import get_chstone, get_others, get_all9
+bms = get_all9()
 for i, bm in enumerate(bms):
   pgm, path = bm
   env_configs['pgm'] = pgm
   env_configs['pgm_dir'] = path
   env_configs['run_dir'] = 'run_'+str(i)
+  env_configs['feature_type'] = 'act_hist'
 
 tune.run_experiments({
     "my_experiment": {
         "run": "PPO",
         "env":HLSEnv,
         "checkpoint_freq": 4,
-        "stop": {"episode_reward_mean": 0},
+        "stop": {"episode_reward_mean": 100000},
         "config": {
-            "sample_batch_size": 100,
-            "train_batch_size": 700,
-            "sgd_minibatch_size": 70,
+            "sample_batch_size": 10,
+            "train_batch_size": 100,
+            "sgd_minibatch_size": 8,
+            "num_sgd_iter": 10,
             "horizon": 12,
             "num_gpus": 2,
-            "num_workers": 7,
+            "num_workers": 5,
             #"lr": tune.grid_search([0.01, 0.001, 0.0001]),
             "env_config": env_configs,
         },
