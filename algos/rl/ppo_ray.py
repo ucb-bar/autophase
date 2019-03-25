1ba308e9f6c5835d2cb661bae471bb724ad30226
Jenny Huang <hqjenny@hotmail.com>
2019-03-25 00:35:22 -0700

    Update ppo config for single pgm

--- algos/rl/ppo_ray.py
+++ algos/rl/ppo_ray.py
@@ -16,23 +16,25 @@ for i, bm in enumerate(bms):
   env_configs['pgm_dir'] = path
   env_configs['run_dir'] = 'run_'+str(i)
   env_configs['feature_type'] = 'act_hist'
+  env_configs['verbose'] = True
 
-tune.run_experiments({
-    "my_experiment": {
-        "run": "PPO",
-        "env":HLSEnv,
-        "checkpoint_freq": 4,
-        "stop": {"episode_reward_mean": 100000},
-        "config": {
-            "sample_batch_size": 10,
-            "train_batch_size": 100,
-            "sgd_minibatch_size": 8,
-            "num_sgd_iter": 10,
-            "horizon": 12,
-            "num_gpus": 2,
-            "num_workers": 5,
-            #"lr": tune.grid_search([0.01, 0.001, 0.0001]),
-            "env_config": env_configs,
-        },
-    },
-})
+  print("Tune for {}".format(pgm))
+  tune.run_experiments({
+      "my_experiment": {
+          "run": "PPO",
+          "env":HLSEnv,
+          "checkpoint_freq": 4,
+          "stop": {"episodes_total": 100},
+          "config": {
+              "sample_batch_size": 10,
+              "train_batch_size": 100,
+              "sgd_minibatch_size": 8,
+              "num_sgd_iter": 10,
+              "horizon": 12,
+              "num_gpus": 1,
+              "num_workers": 5,
+              #"lr": tune.grid_search([0.01, 0.001, 0.0001]),
+              "env_config": env_configs,
+          },
+      },
+  })
