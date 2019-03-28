a05a92c77feade7279bb4b62facc28cc57df8d0a
AmeerHajAli <ameerh@berkeley.edu>
2019-03-28 11:01:41 -0700

    prepare

--- dataset/prepare_train.py
+++ dataset/prepare_train.py
@@ -1,33 +1,52 @@
 import pickle
-def process_csv(filename='train_rand.pkl'): 
+import numpy as np
+def process_csv(filename='train_rand.pkl'):
   entries = []
   with open('random.log') as f:
     lines = f.readlines()
 
     for line in lines:
-      data = line.split('],') 
+      data = line.split('],')
       obs = data[0].replace('[','').split(',')
       obs = list(map(lambda x: int(x), obs))
-            
-      tmp = data[1].split(',') 
-      act = tmp[0]
-      rew = tmp[1]
-      
+
+      tmp = data[1].split(',')
+      act = int(tmp[0])
+      rew = int(tmp[1])
+
       entries.append((obs, act, rew))
-  
+
   output = open(filename, 'wb')
   pickle.dump(entries, output)
   output.close()
 
 def load_data(filename='train_rand.pkl'):
+  num_passes = 45
   pkl_file = open(filename, 'rb')
   entries = pickle.load(pkl_file)
-  obs, act, rew = entries[0]
+  train_data = {}
+  y_data = {}
+  given_passes = []
+  for p in range(num_passes):
+      train_data[p]=[]
+      y_data[p]=[]
+  for (obs,act,rew) in entries:
+    if(int(act) not in given_passes):
+        given_passes.append(int(act))
+    train_data[int(act)].append(obs)
+    good_rew = 0
+    if int(rew) > 0:
+        good_rew = 1
+    y_data[int(act)].append(good_rew)
+  #obs, act, rew = entries[0]
   pkl_file.close()
-  print("{} {} {}".format(obs, act, rew))
+  print("num passes found in pkl file",len(sorted(given_passes)))
+  for p in range(num_passes):
+      train_data[p] = np.array(train_data[p])
+      y_data[p] = np.array(y_data[p])
+  #print("{} {} {}".format(obs, act, rew))
+  return train_data,y_data
+
+#process_csv()
 
 
-process_csv()
-load_data()
-  
-      
