182f8e412fe12ab083e3821db80bce0df33b2d57
Jenny Huang <hqjenny@hotmail.com>
2020-02-20 12:48:15 -0800

    Add env setup script

---
+++ env.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=/scratch/qijing.huang/LegUp/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/scratch/qijing.huang/LegUp/lib:$LD_LIBRARY_PATH
+export PATH=/scratch/qijing.huang/LegUp/bin:$PATH
+export C_INCLUDE_PATH=/usr/lib/gcc/x86_64-linux-gnu/5/include/:/usr/include/linux/:$C_INCLUDE_PATH
+export LD_LIBRARY_PATH=/scratch/anaconda3/lib/:$LD_LIBRARY_PATH
