19590da89bdc52a5b31d21070d07da37431f0824
aelidrissi11 <59592025+aelidrissi11@users.noreply.github.com>
2020-04-14 12:20:26 -0700

    Commented functions in geomean.py

--- gym-hls/gym_hls/envs/geomean.py
+++ gym-hls/gym_hls/envs/geomean.py
@@ -1,5 +1,13 @@
 import numpy as np
 def geomean(iterable):
+    """
+    Examples :
+        >>> print(geomean([1, 3, 27]))
+        4.32674871092
+           
+        >>> print(geomean([1,9,5,6,6,7])
+        4.73989632394
+	"""
     a = np.array(iterable).astype(float)
     prod = a.prod()
     prod = -prod if prod < 0 else prod
